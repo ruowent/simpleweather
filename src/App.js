0bab5d178e6c9c2bbc36070ee70ec61925cae92a
Ruowen Tang <ruowen.tang@gmail.com>
2021-07-26 23:45:04 -0400

    API call to retrive the search result and display weather for current city

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { kelvinToCelsius } from './components/helpers/weatherHelpers';
+
+import './App.scss';
 
 function App() {
   const [searchData, setSearchData] = useState({
     city: ""
   });
+  const [currentCity, setCurrentCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+
   const onSubmit = (searchData) => {
     setCurrentCity(searchData.city)
   }
@@ -19,6 +26,18 @@ function App() {
 
     setSearchData({ ...searchData, [name]: value });
   };
+
+  useEffect(() => {
+    if (currentCity) {
+      const APIKEY = process.env.REACT_APP_APIKEY;
+      const url = `//api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${APIKEY}`;
+      axios
+        .get(url)
+        .then(res => setWeatherData(res.data));
+    }
+  }, [currentCity])
+  console.log(weatherData);
+
   return (
     <div className="App">
       Weather app
@@ -31,6 +50,11 @@ function App() {
           onChange={handleChange}
         />
       </form>
+      <section className="CurrentWeather">
+        {weatherData && <h1>Current Weather for {currentCity}</h1>}
+        {weatherData && <h2>{kelvinToCelsius(weatherData.main.temp)}</h2>}
+
+      </section>
     </div>
   );
 }

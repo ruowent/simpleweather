74dfbb4ae241afcd9fa967cfaf495edbe06a51b1
Ruowen Tang <ruowen.tang@gmail.com>
2021-07-26 23:44:17 -0400

    Form setup to search city name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
 
 function App() {
+  const [searchData, setSearchData] = useState({
+    city: ""
+  });
+  const onSubmit = (searchData) => {
+    setCurrentCity(searchData.city)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(searchData);
+  };
+
+  const handleChange = (event) => {
+    console.log(event.target);
+    const { name, value } = event.target;
+
+    setSearchData({ ...searchData, [name]: value });
+  };
   return (
     <div className="App">
       Weather app
+      <form className="weather-search" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="city"
+          placeholder="Enter a city name"
+          value={searchData.city}
+          onChange={handleChange}
+        />
+      </form>
     </div>
   );
 }

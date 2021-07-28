a0ddb2530e09d57503d971a97c22660d516c08a6
Ruowen Tang <ruowen.tang@gmail.com>
2021-07-28 00:35:53 -0400

    Display weather data for each day

---
+++ src/components/Weather.jsx
@@ -0,0 +1,21 @@
+
+export default function WeatherCards({ weatherData }) {
+  const { min, max, day } = weatherData.temp;
+  const { humidity, dt, wind_speed, pop } = weatherData;
+  const { description, icon } = weatherData.weather[0];
+
+  return (
+    <ul>
+      <img alt="weather-icon" src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
+      <li>Date: {dt}</li>
+      <li>Description: {description}</li>
+      <li>Day: {day}°C</li>
+      <li>Minimum: {min}°C</li>
+      <li>Maximum: {max}°C</li>
+      <li>Wind Speed: {wind_speed}km/hr</li>
+      <li>Humidity: {humidity}%</li>
+      <li>Precipitation: {pop}%</li>
+    </ul>
+  )
+
+}
